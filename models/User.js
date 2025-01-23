40695bae396961dd35c55ffd187c42b14f00057f
Michal Gawik <gawlik.zator@o2.pl>
2025-01-23 07:38:28 +0100

    install bcrypt

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-// const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs')
 // const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
@@ -33,6 +33,11 @@ const UserSchema = new mongoose.Schema({
 		enum: ['admin', 'user'],
 		default: 'user',
 	},
+})
+
+UserSchema.pre('save', async function () {
+	const salt = await bcrypt.genSalt(10)
+	this.password = await bcrypt.hash(this.password, salt)
 })
 
 module.exports = mongoose.model('User', UserSchema)
