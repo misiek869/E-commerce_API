028ed50a083e10cb89fa097559c9c51d24104627
Michal Gawik <gawlik.zator@o2.pl>
2025-01-23 06:20:41 +0100

    import CustomAPIError

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,11 +2,11 @@ const User = require('../models/User')
 
 const { StatusCodes } = require('http-status-codes')
 
+const CustomAPIError = require('../errors')
+
 const register = async (req, res) => {
 	const user = await User.create({ ...req.body })
-	res
-		.status(StatusCodes.CREATED)
-		.json({ user: { name: user.name, email: user.email } })
+	res.status(StatusCodes.CREATED).json({ user })
 	res.send(user)
 }
 
