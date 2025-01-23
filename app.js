921e1e4475eaf82bc7ae3b89edd2500fb8b40638
Michal Gawik <gawlik.zator@o2.pl>
2025-01-23 05:24:53 +0100

    create user schema

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+// packages
+const morgan = require('morgan')
+
 // database
 const connectDB = require('./db/connect')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+app.use(morgan('tiny'))
 app.use(express.json())
 
 app.get('/', (req, res) => {
