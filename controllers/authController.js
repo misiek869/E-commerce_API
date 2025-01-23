d6a8f806e6a22aefc941c89e57e3e8668bc26ccf
Michal Gawik <gawlik.zator@o2.pl>
2025-01-23 06:26:22 +0100

    no major changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,9 +5,8 @@ const { StatusCodes } = require('http-status-codes')
 const CustomAPIError = require('../errors')
 
 const register = async (req, res) => {
-	const user = await User.create({ ...req.body })
+	const user = await User.create(req.body)
 	res.status(StatusCodes.CREATED).json({ user })
-	res.send(user)
 }
 
 const login = async (req, res) => {
