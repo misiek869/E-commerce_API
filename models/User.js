c05c8a77d5b635b7475255a085690e6185e3f94c
Michal Gawik <gawlik.zator@o2.pl>
2025-01-23 06:49:04 +0100

    a

--- models/User.js
+++ models/User.js
@@ -12,12 +12,12 @@ const UserSchema = new mongoose.Schema({
 	},
 	email: {
 		type: String,
+		unique: true,
 		required: [true, 'Please provide email'],
 		// match: [
 		// 	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 		// 	'Please provide a valid email',
 		// ],
-		unique: true,
 		validate: {
 			validator: validator.isEmail,
 			message: 'Please provide valid email',
