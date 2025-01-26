f4d4da6e8ec64fa17f0856a460559126437a5d2e
Michal Gawik <gawlik.zator@o2.pl>
2025-01-26 17:01:16 +0100

    update getAllUsers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,26 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+
 const getAllUsers = async (req, res) => {
-	res.send('all users')
+	const users = await User.find({ role: 'user' }).select('-password')
+
+	res.send(users)
 }
+
 const getSingleUser = async (req, res) => {
-	res.send('single user')
+	const {
+		params: { id: userId },
+	} = req
+
+	const user = await User.findOne({
+		_id: userId,
+	})
+	// if (!user) {
+	// 	throw new NotFoundError(`No user with id ${jobId}`)
+	// }
+	res.status(StatusCodes.OK).json({ user })
+
+	// res.send(user)
 }
 const showCurrentUser = async (req, res) => {
 	res.send('current user')
