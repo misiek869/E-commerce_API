c851693e9daf128fce9eb31954b4a3e360554bc7
Michal Gawik <gawlik.zator@o2.pl>
2025-01-26 17:03:50 +0100

    add NotFoundError

--- errors/index.js
+++ errors/index.js
@@ -1,9 +1,11 @@
 const CustomAPIError = require('./custom-api')
 const BadRequestError = require('./bad-request')
 const UnauthenticatedError = require('./unauthenticated')
+const NotFoundError = require('./NotFoundError')
 
 module.exports = {
 	CustomAPIError,
 	BadRequestError,
 	UnauthenticatedError,
+	NotFoundError,
 }
