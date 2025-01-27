74fa5746491385c01451da6a5dbcaf3b75e35d0c
Michal Gawik <gawlik.zator@o2.pl>
2025-01-27 07:46:36 +0100

    add conditions to check if user can see other users

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,9 +1,15 @@
-// const {} = require('../errors')
+const { UnauthorizedError } = require('../errors')
 
 const checkPermissions = (requestUser, resourceUserId) => {
-	console.log(requestUser)
-	console.log(resourceUserId)
-	console.log(typeof resourceUserId)
+	// console.log(requestUser)
+	// console.log(resourceUserId)
+	// console.log(typeof resourceUserId)
+
+	if (requestUser.role === 'admin') return
+
+	if (requestUser.userId === resourceUserId.toString()) return
+
+	throw new UnauthorizedError(`You can't access this route`)
 }
 
 module.exports = checkPermissions
