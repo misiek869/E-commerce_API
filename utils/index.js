c36fea940eb473b501496490946d0e219aaa18c2
Michal Gawik <gawlik.zator@o2.pl>
2025-01-27 07:37:55 +0100

    add checkPermissions to export

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,11 @@
 const { createJWT, isTokenValid, addCookiesToResponse } = require('./jwt')
 const { createTokenUser } = require('./createTokenUser')
+const checkPermissions = require('./checkPermissions')
 
 module.exports = {
 	createJWT,
 	isTokenValid,
 	addCookiesToResponse,
 	createTokenUser,
+	checkPermissions,
 }
