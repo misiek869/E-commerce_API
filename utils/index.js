b17a34ee84b18bb4b4c6fd0a4721cdb36e5f408d
Michal Gawik <gawlik.zator@o2.pl>
2025-01-27 06:51:42 +0100

    add createTokenUser to exports

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,9 @@
 const { createJWT, isTokenValid, addCookiesToResponse } = require('./jwt')
+const { createTokenUser } = require('./createTokenUser')
 
 module.exports = {
 	createJWT,
 	isTokenValid,
 	addCookiesToResponse,
+	createTokenUser,
 }
