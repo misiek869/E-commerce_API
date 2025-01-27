381530797da6fd6822149f5d1165c34b5422c64e
Michal Gawik <gawlik.zator@o2.pl>
2025-01-27 07:20:59 +0100

    refactor updateUser to use save()

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,18 +40,37 @@ const updateUser = async (req, res) => {
 		throw new BadRequestError('Please provide both values')
 	}
 
-	const user = await User.findOneAndUpdate(
-		{ _id: req.user.userId },
-		{ email, name },
-		{
-			new: true,
-			runValidators: true,
-		}
-	)
+	const user = await User.findOne({ _id: req.user.userId })
+
+	user.email = email
+	user.name = name
+
+	await user.save()
+
 	const tokenUser = createTokenUser(user)
 	addCookiesToResponse({ res, user: tokenUser })
 	res.status(StatusCodes.OK).json({ user: tokenUser })
 }
+
+// const updateUser = async (req, res) => {
+// 	const { name, email } = req.body
+
+// 	if (!name || !email) {
+// 		throw new BadRequestError('Please provide both values')
+// 	}
+
+// 	const user = await User.findOneAndUpdate(
+// 		{ _id: req.user.userId },
+// 		{ email, name },
+// 		{
+// 			new: true,
+// 			runValidators: true,
+// 		}
+// 	)
+// 	const tokenUser = createTokenUser(user)
+// 	addCookiesToResponse({ res, user: tokenUser })
+// 	res.status(StatusCodes.OK).json({ user: tokenUser })
+// }
 
 const updateUserPassword = async (req, res) => {
 	const { newPassword, oldPassword } = req.body
