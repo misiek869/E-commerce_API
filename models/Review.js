ed379a60bf5231ac645e0cc240836baecc1f344c
Michal Gawik <gawlik.zator@o2.pl>
2025-01-28 09:19:05 +0100

    protect before leaving more than 1 review

--- models/Review.js
+++ models/Review.js
@@ -35,4 +35,6 @@ const ReviewSchema = new mongoose.Schema(
 	{ timestamps: true }
 )
 
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true })
+
 module.exports = mongoose.model('Review', ReviewSchema)
