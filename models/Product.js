ef9c464cd4d1ecfd9a509dda2a380f2328609777
Michal Gawik <gawlik.zator@o2.pl>
2025-01-28 05:53:16 +0100

    create product model

---
+++ models/Product.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'Please provide product name'],
+			maxlength: [100, `Name can't be longer than 100 characters`],
+			minlength: 3,
+		},
+		price: {
+			type: Number,
+			required: [true, 'Please provide product price'],
+			default: 0,
+		},
+		description: {
+			type: String,
+			required: [true, 'Please provide product description'],
+			maxlength: [1000, `Description can't be longer than 1000 characters`],
+			minlength: 10,
+		},
+		image: {
+			type: String,
+			// default:'/default-photo'
+		},
+		category: {
+			type: String,
+			required: [true, 'Please provide product category'],
+			enum: ['office', 'kitchen', 'bedroom'],
+		},
+		company: {
+			type: String,
+			required: [true, 'Please provide company'],
+			enum: {
+				values: ['Cozy Corner Furniture', 'Modern Living', 'Furniture Haven'],
+				message: '{VALUE} is not supported',
+			},
+		},
+		colors: {
+			type: [String],
+			required: true,
+		},
+		featured: {
+			type: Boolean,
+			default: false,
+		},
+		freeShipping: {
+			type: Boolean,
+			default: false,
+		},
+		inventory: {
+			type: Number,
+			required: true,
+			default: 15,
+		},
+		averageRating: {
+			type: Number,
+			default: 0,
+		},
+		user: {
+			type: mongoose.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+	},
+	{ timestamps: true }
+)
+
+module.exports = mongoose.model('Product', ProductSchema)
