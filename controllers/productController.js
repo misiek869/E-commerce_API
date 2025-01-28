065eb6a73ee523220824843ff1f422c6205a40da
Michal Gawik <gawlik.zator@o2.pl>
2025-01-28 06:03:12 +0100

    create first functions

---
+++ controllers/productController.js
@@ -0,0 +1,32 @@
+const Product = require('../models/Product')
+const { StatusCodes } = require('http-status-codes')
+const {
+	NotFoundError,
+	BadRequestError,
+	UnauthenticatedError,
+} = require('../errors')
+
+const {
+	createTokenUser,
+	addCookiesToResponse,
+	checkPermissions,
+} = require('../utils')
+
+const createProduct = async (req, res) => {
+	res.send('create')
+}
+const getAllProducts = async (req, res) => {
+	res.send('get all')
+}
+const getSingleProduct = async (req, res) => {
+	res.send('get single')
+}
+const updateProduct = async (req, res) => {
+	res.send('update')
+}
+const deleteProduct = async (req, res) => {
+	res.send('delete')
+}
+const uploadImage = async (req, res) => {
+	res.send('upload image')
+}
