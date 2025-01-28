65b6dbf3a688b53307e87275b2e3abbec5c157c5
Michal Gawik <gawlik.zator@o2.pl>
2025-01-28 09:16:17 +0100

    create review model

---
+++ models/Review.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const ReviewSchema = new mongoose.Schema(
+	{
+		rating: {
+			type: Number,
+			min: 1,
+			max: 5,
+			required: [true, 'Please provide rating'],
+		},
+		title: {
+			type: String,
+			trim: true,
+			required: [true, 'Please provide title'],
+			maxlength: [50, `Title can't be longer than 100 characters`],
+			minlength: 10,
+		},
+		comment: {
+			type: String,
+			required: [true, 'Please provide commnet'],
+			maxlength: [300, `Title can't be longer than 300 characters`],
+			minlength: 10,
+		},
+		user: {
+			type: mongoose.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		product: {
+			type: mongoose.Types.ObjectId,
+			ref: 'Product',
+			required: true,
+		},
+	},
+	{ timestamps: true }
+)
+
+module.exports = mongoose.model('Review', ReviewSchema)
